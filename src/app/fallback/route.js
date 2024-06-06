aca81343ded9ae3fd3c3d8241dcb615136b5048f
FEAZ\PC <bansaohuy333@gmail.com>
2024-06-06 09:27:17 +0700

    hyu-060624 update

--- src/app/fallback/route.js
+++ src/app/fallback/route.js
@@ -21,129 +21,205 @@ ffmpeg.setFfprobePath(
 );
 
 const dirPath_ = process.env.DIR_PATH;
-const instruPath = `${dirPath_}\\instru`;
-const popPath = `${dirPath_}\\pop`;
-const screenPath = `${dirPath_}\\screen`;
 const newDirPath = `${dirPath_}\\020624-hyu`;
 const btsPath = `${newDirPath}\\bts`;
-const instruFormatedPath = `${instruPath}\\formated`;
-const popformatedPath = `${popPath}\\formated`;
-const screenformatedPath = `${screenPath}\\formated`;
+
+const instruPath = `${dirPath_}\\instru`;
+const instruSelectedPath = `${instruPath}\\selected`;
+const instruPriorityPath = `${instruPath}\\priority`;
+// const instruFormatedPath = `${instruPath}\\formated`;
+
+const popPath = `${dirPath_}\\pop`;
+const popFormattedPath = `${popPath}\\formated`;
+
+const screenPath = `${dirPath_}\\screen`;
+const screenFormattedPath = `${screenPath}\\formated`;
 
 export async function GET() {
-  // asd();
-  await bsd();
+  await oneClick();
+
   return Response.json({ rep: "hello" });
 }
 
-async function bsd() {
-  myMkdir(screenformatedPath);
+async function oneClick() {
+  const playlist = await getPlayList();
+  await createPlayListTimeFile(playlist);
+  await createTmpAudioFile(playlist);
 
-  const files = await fs.readdir(screenPath);
-  const filesFormated = await fs.readdir(screenformatedPath);
+  // await createTmpScreenFile();
+}
+async function getPlayList() {
+  const files = await fs.readdir(instruSelectedPath);
 
-  const filesNotFormatedMp4 = await files
-    .filter((file) => [".mp4"].includes(path.extname(file).toLowerCase()))
-    .filter((file) => !filesFormated.includes(file))
+  const isShuffle = true;
+  if (isShuffle) {
+    shuffleArray(files);
+  }
+
+  const mp3Files = await files
+    .filter((file) =>
+      [
+        ".mp3",
+        // , ".mp4"
+      ].includes(path.extname(file).toLowerCase())
+    )
     .map((file) => {
       return {
         name: removeFileExtension(file),
         fullName: file,
-        absPath: `${screenPath}\\${file}`,
+        absPath: `${instruSelectedPath}\\${file}`,
       };
     });
 
-  recursiveffmpeg(filesNotFormatedMp4);
-}
-
-function recursiveffmpeg(screenFiles) {
-  subRecursiveffmpeg(screenFiles, 1, screenFiles.length);
-
-  function subRecursiveffmpeg(files, currentIndex, lengthFile) {
-    const curFile = files.shift();
-    const inputFile = curFile.absPath;
-    console.log("inputFile: ", inputFile);
-    const outputFile = `${screenformatedPath}\\${curFile.fullName}`;
-    console.log("outputFile: ", outputFile);
-    ffmpeg(inputFile)
-      .outputOptions(
-        "-safe",
-        "1",
-        "-r",
-        "24",
-        "-s",
-        "1920x1080",
-        "-c:v",
-        "libx264",
-        "-b:v",
-        "10000k",
-        "-c:a",
-        "copy"
-      )
-      .on("start", (cmd) => {
-        // console.log("Audio with command: " + cmd);
-        console.log(`${currentIndex}/${lengthFile} format starting...`);
-      })
-      .on("error", (err) => {
-        console.error("An error occurred: " + err.message);
-      })
-      .on("end", () => {
-        // console.log("Conversion finished");
-        if (currentIndex < lengthFile) {
-          subRecursiveffmpeg(screenFiles, ++currentIndex, lengthFile);
-        } else {
-          console.log("kết thúc format");
-        }
-      })
-      .save(outputFile);
+  //time config
+  let initStartTime = 0.0;
+  let currentStartTime = 0.0;
+  for (const file of mp3Files) {
+    file.durationSecond = (
+      await parseFile(`${instruSelectedPath}\\${file.fullName}`)
+    ).format.duration;
+    file.startTimeSecond = initStartTime + currentStartTime;
+    file.startTimeFormat = formatDuration(file.startTimeSecond);
+    currentStartTime += file.durationSecond;
   }
+
+  return mp3Files;
 }
+async function createPlayListTimeFile(playlist) {
+  const playlistFilePath = `${newDirPath}\\playlist.txt`;
+  // await myMkdir(newDirPath);
 
-async function asd() {
-  const inputVideo = `${screenPath}\\4k.mp4`; // Đường dẫn đến video đầu vào
-  const outputVideo = `${newDirPath}\\sample.mp4`; // Đường dẫn đến video đầu ra
+  const listingMusicDocs = playlist
+    .map((player) => {
+      return player.startTimeFormat.concat(
+        " ",
+        removeFileExtension(player.name)
+      );
+    })
+    .join("\n");
 
-  // const audioDuration = (await parseFile(inputVideo)).format;
-  // console.log("audioDuration: ", audioDuration);
+  // Ghi nội dung vào tệp
+  await fs.writeFile(playlistFilePath, listingMusicDocs, {
+    flag: "w",
+  });
+}
+async function createTmpAudioFile(playlist) {
+  // Path to the output audio file
+  const outputAudio = `${newDirPath}\\tmp_audio.mp3`;
 
-  // ffmpeg.ffprobe(inputVideo, function (err, metadata) {
-  //   console.log(metadata);
-  // });
+  // Create a temporary file list
+  const tempFileList = `${newDirPath}\\tmp_audio_files.txt`;
+  const fileListContent = playlist
+    .map(({ absPath }) => `file '${absPath}'`)
+    .join("\n");
 
-  // return;
-  ffmpeg(inputVideo)
-    .outputOptions(
-      "-safe",
-      "1",
-      "-r",
-      "24",
-      "-s",
-      "1920x1080",
-      "-c:v",
-      "libx264",
-      "-b:v",
-      "10000k",
-      "-c:a",
-      "copy"
-    )
+  fs.writeFile(tempFileList, fileListContent);
+
+  // Merge audio files using the concat filter
+  ffmpeg()
+    .input(tempFileList)
+    .inputOptions(["-f", "concat", "-safe", "0"])
+    .outputOptions("-c", "copy")
     .on("start", (cmd) => {
       // console.log("Audio with command: " + cmd);
-      console.log("Format starting...");
+      console.log("Audio starting...");
+    })
+    .on("progress", (progress) => {
+      // console.log("Processing Audio: " + progress.percent + "% done");
+    })
+    .on("end", async () => {
+      console.log("Audio finished successfully");
+      fs.unlink(tempFileList); // Clean up the temporary file
+      await createTmpScreenFile(playlist);
+    })
+    .on("error", (err) => {
+      console.error("An error occurred: " + err.message);
+      fs.unlink(tempFileList); // Clean up the temporary file in case of error
+    })
+    .save(outputAudio);
+}
+async function createTmpScreenFile(playlist) {
+  //get audio duration
+  const audioPath = `${newDirPath}\\tmp_audio.mp3`;
+  const audioDuration = (await parseFile(audioPath)).format.duration;
+  if (!audioDuration) {
+    console.error("File not found ");
+    return;
+  }
+  //get screen file - duration, path, name
+  const files = await fs.readdir(screenformatedPath);
+  const mp4Files = files.filter((file) =>
+    [".mp4"].includes(path.extname(file).toLowerCase())
+  );
+  //shuffle list
+  const mp4FilesShuffled = shuffleArray(mp4Files);
+  const mp4FilesShuffledPath = mp4FilesShuffled.map(
+    (fileName) => `${screenformatedPath}\\${fileName}`
+  );
+  // Path to the output audio file
+  const outputScreen = `${newDirPath}\\tmp_screen.mp4`;
+  // Create a temporary file list
+  const tempFileList = `${newDirPath}\\tmp_screen_files.txt`;
+  const fileListContent = mp4FilesShuffledPath
+    .map((filePath) => `file '${filePath}'`)
+    .join("\n");
+
+  fs.writeFile(tempFileList, fileListContent);
+
+  // Merge screen files using the concat filter
+  ffmpeg()
+    .input(tempFileList)
+    .inputOptions(["-f", "concat", "-safe", "0"])
+    .outputOptions("-c", "copy")
+    .duration(audioDuration)
+    .on("start", (cmd) => {
+      // console.log("Screen ffmpeg with command: " + commandLine);
+      console.log("Screen starting...");
+    })
+    .on("progress", (progress) => {
+      // console.log("Processing : " + progress.percent + "% done");
+    })
+    .on("end", async () => {
+      console.log("Screen finished successfully");
+      fs.unlink(tempFileList); // Clean up the temporary file
+      await createEndProduct();
+    })
+    .on("error", (err) => {
+      console.error("An error occurred: " + err.message);
+      fs.unlink(tempFileList); // Clean up the temporary file in case of error
+    })
+    .save(outputScreen);
+}
+async function createEndProduct() {
+  const inputVideo = `${newDirPath}\\tmp_screen.mp4`;
+  const newAudio = `${newDirPath}\\tmp_audio.mp3`;
+  const outputVideo = `${newDirPath}\\endprod.mp4`;
+
+  ffmpeg(inputVideo)
+    .input(newAudio)
+    .outputOptions("-c:v", "copy", "-map", "0:v:0", "-map", "1:a:0")
+    .audioCodec("aac")
+    .on("start", (cmd) => {
+      console.error("End product started!");
     })
     .on("error", (err) => {
       console.error("An error occurred: " + err.message);
     })
     .on("end", () => {
-      console.log("Conversion finished");
+      console.log("End product finish!");
+      cleanUp();
     })
     .save(outputVideo);
 }
+async function cleanUp() {
+  const filesClean = [
+    `${newDirPath}\\tmp_audio.mp3`,
+    `${newDirPath}\\tmp_screen.mp4`,
+  ];
 
-// const xample = a.outputOptions(
-//   '-safe', '1' ,
-//   '-r', '24' ,
-//   '-s', '1920x1080' ,
-//   '-c:a', 'copy',
-//   '-c:v', 'libx264',
-//   '-b:v', '10000k'
-// )
+  for (const file of filesClean) {
+    fs.unlink(file);
+  }
+
+  console.log("Clean!");
+}
--- src/app/render/route.js
+++ src/app/render/route.js
@@ -21,14 +21,19 @@ ffmpeg.setFfprobePath(
 );
 
 const dirPath_ = process.env.DIR_PATH;
-const instruPath = `${dirPath_}\\instru`;
-const popPath = `${dirPath_}\\pop`;
-const screenPath = `${dirPath_}\\screen`;
 const newDirPath = `${dirPath_}\\020624-hyu`;
 const btsPath = `${newDirPath}\\bts`;
-const instruFormatedPath = `${instruPath}\\formated`;
-const popformatedPath = `${popPath}\\formated`;
-const screenformatedPath = `${screenPath}\\formated`;
+
+const instruPath = `${dirPath_}\\instru`;
+const instruSelectedPath = `${instruPath}\\selected`;
+const instruPriorityPath = `${instruPath}\\priority`;
+// const instruFormatedPath = `${instruPath}\\formated`;
+
+const popPath = `${dirPath_}\\pop`;
+const popFormattedPath = `${popPath}\\formated`;
+
+const screenPath = `${dirPath_}\\screen`;
+const screenFormattedPath = `${screenPath}\\formated`;
 
 export async function GET() {
   await oneClick();
@@ -36,14 +41,121 @@ export async function GET() {
   return Response.json({ rep: "hello" });
 }
 
+export async function POST(request) {
+  const order = await request.json();
+  console.log("order: ", order);
+  const result = await getPlayList(order);
+  console.log("result : ", result);
+  return Response.json({ rep: "hello" });
+}
+
 async function oneClick() {
   const playlist = await getPlayList();
-
   await createPlayListTimeFile(playlist);
-
   await createTmpAudioFile(playlist);
 
-  // await createEndProduct(playlist);
+  // await createTmpScreenFile();
+}
+async function getPlayList(order) {
+  if (order) {
+    return getPlayListOnOrder(order);
+  }
+
+  const files = await fs.readdir(instruSelectedPath);
+
+  const isShuffle = true;
+  if (isShuffle) {
+    shuffleArray(files);
+  }
+
+  const mp3Files = await files
+    .filter((file) => [".mp3"].includes(path.extname(file).toLowerCase()))
+    .map((file) => {
+      return {
+        name: removeFileExtension(file),
+        fullname: file,
+        absPath: `${instruSelectedPath}\\${file}`,
+      };
+    });
+
+  await durationExpand(mp3Files, instruSelectedPath);
+
+  return mp3Files;
+}
+async function getPlayListOnOrder(order) {
+  let audioFiles = [];
+
+  if (order.priorityPicked > 0) {
+    const priorityFiles = (await fs.readdir(instruPriorityPath))
+      .filter((file) => [".mp3"].includes(path.extname(file).toLowerCase()))
+      .map((fileFullName) => ({
+        name: removeFileExtension(fileFullName),
+        fullname: fileFullName,
+        absPath: `${instruPriorityPath}\\${fileFullName}`,
+      }));
+
+    if (!priorityFiles || priorityFiles.length < order.priorityPicked) {
+      return [];
+    }
+
+    const priorityFilesSelected = priorityFiles.slice(0, order.priorityPicked);
+    audioFiles.unshift(...priorityFilesSelected);
+
+    if (order.totalPlaylistBySongNumbers) {
+      order.totalPlaylistBySongNumbers -= priorityFilesSelected.length;
+    }
+  }
+
+  if (order.isSelect) {
+    const fillFilesSelect = (await fs.readdir(instruSelectedPath))
+      .filter((file) => [".mp3"].includes(path.extname(file).toLowerCase()))
+      .map((fileFullName) => ({
+        name: removeFileExtension(fileFullName),
+        fullname: fileFullName,
+        absPath: `${instruSelectedPath}\\${fileFullName}`,
+      }));
+
+    const priorityFilesSelected = fillFilesSelect.slice(
+      0,
+      order.totalPlaylistBySongNumbers
+    );
+    audioFiles.push(...priorityFilesSelected);
+  }
+  await durationExpand(audioFiles);
+
+  return audioFiles;
+}
+async function setMetadataFile() {}
+async function metadataExpand(files, path) {}
+async function durationExpand(files) {
+  //time config
+  let initStartTime = 0.0;
+  let currentStartTime = 0.0;
+  for (const file of files) {
+    file.durationSecond = (await parseFile(file.absPath)).format.duration;
+    file.startTimeSecond = initStartTime + currentStartTime;
+    file.startTimeFormat = formatDuration(file.startTimeSecond);
+    currentStartTime += file.durationSecond;
+  }
+}
+
+async function createPlayListTimeFile(playlist) {
+  const playlistFilePath = `${newDirPath}\\playlist.txt`;
+  // await myMkdir(newDirPath);
+
+  const listingMusicDocs = playlist
+    .map((player) => {
+      return player.startTimeFormat.concat(
+        " ",
+        removeFileExtension(player.name)
+      );
+    })
+    .join("\n");
+
+  // Ghi nội dung vào tệp
+  await fs.writeFile(playlistFilePath, listingMusicDocs, {
+    flag: "w",
+  });
 }
 async function createTmpAudioFile(playlist) {
   // Path to the output audio file
@@ -85,34 +197,28 @@ async function createTmpScreenFile(playlist) {
   const audioPath = `${newDirPath}\\tmp_audio.mp3`;
   const audioDuration = (await parseFile(audioPath)).format.duration;
   if (!audioDuration) {
-    console.error("Found not file");
+    console.error("File not found ");
     return;
   }
-
   //get screen file - duration, path, name
-  const files = await fs.readdir(screenPath);
+  const files = await fs.readdir(screenFormattedPath);
   const mp4Files = files.filter((file) =>
     [".mp4"].includes(path.extname(file).toLowerCase())
   );
-
   //shuffle list
   const mp4FilesShuffled = shuffleArray(mp4Files);
   const mp4FilesShuffledPath = mp4FilesShuffled.map(
-    (fileName) => `${screenPath}\\${fileName}`
+    (fileName) => `${screenFormattedPath}\\${fileName}`
   );
-
   // Path to the output audio file
   const outputScreen = `${newDirPath}\\tmp_screen.mp4`;
-
   // Create a temporary file list
   const tempFileList = `${newDirPath}\\tmp_screen_files.txt`;
   const fileListContent = mp4FilesShuffledPath
     .map((filePath) => `file '${filePath}'`)
     .join("\n");
-
   fs.writeFile(tempFileList, fileListContent);
 
-  // return;
   // Merge screen files using the concat filter
   ffmpeg()
     .input(tempFileList)
@@ -136,61 +242,6 @@ async function createTmpScreenFile(playlist) {
       fs.unlink(tempFileList); // Clean up the temporary file in case of error
     })
     .save(outputScreen);
-}
-async function createPlayListTimeFile(playlist) {
-  const playlistFilePath = `${newDirPath}\\playlist.txt`;
-  // await myMkdir(newDirPath);
-
-  const listingMusicDocs = playlist
-    .map((player) => {
-      return player.startTimeFormat.concat(
-        " ",
-        removeFileExtension(player.name)
-      );
-    })
-    .join("\n");
-
-  // Ghi nội dung vào tệp
-  await fs.writeFile(playlistFilePath, listingMusicDocs, {
-    flag: "w",
-  });
-}
-async function getPlayList() {
-  const files = await fs.readdir(instruPath);
-
-  const isShuffle = true;
-  if (isShuffle) {
-    shuffleArray(files);
-  }
-
-  const mp3Files = await files
-    .filter((file) =>
-      [
-        ".mp3",
-        // , ".mp4"
-      ].includes(path.extname(file).toLowerCase())
-    )
-    .map((file) => {
-      return {
-        name: removeFileExtension(file),
-        fullName: file,
-        absPath: `${instruPath}\\${file}`,
-      };
-    });
-
-  //time config
-  let initStartTime = 0.0;
-  let currentStartTime = 0.0;
-  for (const file of mp3Files) {
-    file.durationSecond = (
-      await parseFile(`${instruPath}\\${file.fullName}`)
-    ).format.duration;
-    file.startTimeSecond = initStartTime + currentStartTime;
-    file.startTimeFormat = formatDuration(file.startTimeSecond);
-    currentStartTime += file.durationSecond;
-  }
-
-  return mp3Files;
 }
 async function createEndProduct() {
   const inputVideo = `${newDirPath}\\tmp_screen.mp4`;
